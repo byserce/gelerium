dfe31471e9ee7e113a8b56a323a80ee18eec666c
Atakan Serçe <atakan.serce1@gmail.com>
2025-12-03 12:03:48 +0000

    [   {     "id": "1283399946",     "baslik": "SAFKAN BENZİNLİ KAPUT BOYAS

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -5,12 +5,12 @@ import type { Car } from './types';
 
 export const cars: Car[] = [
   {
-    id: '1',
+    id: '1283399946',
     sahibindenId: '1283399946',
     title: 'SAFKAN BENZİNLİ KAPUT BOYASIZ',
     price: 319900,
-    year: 2018,
-    km: 110000,
+    year: 1999,
+    km: 298500,
     brand: 'Opel',
     model: 'Astra',
     imageUrls: [
@@ -19,26 +19,26 @@ export const cars: Car[] = [
     listingUrl: 'https://www.sahibinden.com/ilan/vasita-otomobil-opel-safkan-benzinli-kaput-boyasiz-1283399946/detay',
   },
   {
-    id: '2',
+    id: '1286367961',
     sahibindenId: '1286367961',
     title: 'EMSALSİZ AUDİ OTOMOTİK',
     price: 575000,
-    year: 2015,
-    km: 150000,
+    year: 2006,
+    km: 298500,
     brand: 'Audi',
-    model: 'A3',
+    model: 'A4',
     imageUrls: [
       "https://i0.shbdn.com/photos/36/79/61/lthmb_1286367961r59.avif"
     ],
     listingUrl: 'https://www.sahibinden.com/ilan/vasita-otomobil-audi-emsalsiz-audi-otomotik-1286367961/detay',
   },
   {
-    id: '3',
+    id: '1286336583',
     sahibindenId: '1286336583',
     title: 'LANSMAN RENK REFLEX GRİ DÜŞÜK KMLİ',
     price: 1099000,
-    year: 2021,
-    km: 45000,
+    year: 2012,
+    km: 169000,
     brand: 'Honda',
     model: 'Civic',
     imageUrls: [
@@ -47,14 +47,14 @@ export const cars: Car[] = [
     listingUrl: 'https://www.sahibinden.com/ilan/vasita-otomobil-honda-lansman-renk-reflex-gri-dusuk-kmli-1286336583/detay',
   },
   {
-    id: '4',
+    id: '1284402663',
     sahibindenId: '1284402663',
     title: 'AĞIR BAKIMLARI YAPILMIŞ OTOMOTİK',
     price: 625000,
-    year: 2017,
-    km: 95000,
+    year: 2008,
+    km: 318500,
     brand: 'Honda',
-    model: 'Jazz',
+    model: 'Civic',
     imageUrls: [
       "https://i0.shbdn.com/photos/40/26/63/lthmb_1284402663iai.avif"
     ],
