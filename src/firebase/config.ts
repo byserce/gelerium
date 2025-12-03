6a92046f7a62a737d7cdfe783c8e9124422c5594
Atakan Serçe <atakan.serce1@gmail.com>
2025-12-03 17:37:37 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5014137104-8ce57",
+  "appId": "1:715795746218:web:8823a92dbb9a45702d6fd3",
+  "apiKey": "AIzaSyCSjYjxYLQRgPlf4ONyY64hVhnuydm63QY",
+  "authDomain": "studio-5014137104-8ce57.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "715795746218"
+};
